00202961764cf49a74d994e7cf51d804749de5d2
rivendo21 <abdelrahman2729@gmail.com>
2025-11-20 10:50:12 +0200

    Update axios.js

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "/api",
+  baseURL: "https://full-stack-e-commerce-2-upln.onrender.com/api",
   withCredentials: true,
 });
 
