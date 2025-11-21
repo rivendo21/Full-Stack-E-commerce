7d8ae3cc0c9e45474717d1f0ae5ceae596b7549f
rivendo21 <abdelrahman2729@gmail.com>
2025-11-21 04:04:34 +0200

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import cors from "cors";
+
 import path from "path";
 import { fileURLToPath } from "url";
 
@@ -24,13 +24,8 @@ const PORT = process.env.PORT || 5000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Middleware
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
-    credentials: true,
-  })
-);
+
+
 app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
 
