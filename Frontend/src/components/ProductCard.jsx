7cd7b229d79431dacbcc2b4897e470f99e0cb025
rivendo21 <abdelrahman2729@gmail.com>
2025-11-25 22:07:24 +0200

    Update ProductCard.jsx

--- Frontend/src/components/ProductCard.jsx
+++ Frontend/src/components/ProductCard.jsx
@@ -5,15 +5,13 @@ import { useCartStore } from "../stores/useCartStore";
 const ProductCard = ({ product }) => {
   const { user } = useUserStore();
   const { addToCart } = useCartStore();
-  const handleAddToCart = () => {
-    if (!user) {
-      toast.error("Please login to add products to cart", { id: "login" });
-      return;
-    } else {
-      // add to cart
-      addToCart(product);
-    }
-  };
+ const handleAddToCart = () => {
+  if (!user) {
+    toast.error("Please login to add products to cart", { id: "login" });
+    return;
+  }
+  addToCart({ productId: product._id });
+};
 
   return (
     <div className="flex w-full relative flex-col overflow-hidden rounded-lg border border-gray-700 shadow-lg">
